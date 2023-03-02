590cb2b7377915ecc7df068a84443b349afa47b7
Joon Jang <joon.jang@teck.com>
2023-03-02 09:42:44 -0800

    can see CosmosDB and CSV side by side now

--- server/src/services/access-cosmos/index.ts
+++ server/src/services/access-cosmos/index.ts
@@ -8,23 +8,21 @@ import { BINDINGS } from '../../bindings';
 export class AccessCosmosService {
   private cosmosDbContainer: CosmosDbContainer;
 
-
   constructor(@inject(BINDINGS.CosmosDbService) private cosmosDbService: CosmosDbService) {
     this.cosmosDbContainer = this.cosmosDbService.database.container("container-pro-hvc-bo-hour-utilization");
   }
   
   async retrieve(){
 
-    
-
-    const QUERY = "SELECT * FROM c ORDER BY c._ts DESC OFFSET 0 LIMIT 3";
+    const QUERY = "SELECT * FROM c ORDER BY c._ts DESC OFFSET 0 LIMIT 6";
     try {
       const {resources: items } = await this.cosmosDbContainer.items.query({query: QUERY}).fetchAll();
 
       if (!items){
         return "No response";
       }
-      console.log(items);
+      // console.log(items);
+      console.log("SUCCESS");
       return JSON.stringify(items);
     } catch(e) {
       return "ERROR: " + e;
